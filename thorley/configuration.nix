3ad656351cb8c25a13f5f098ddd8e227ddd03a0b
Skye <me@skye.vg>
2024-01-31 12:41:05 +0900

    box64(woo)

--- thorley/configuration.nix
+++ thorley/configuration.nix
@@ -56,6 +56,13 @@ in {
 
   virtualisation.waydroid.enable = true;
 
+  boot.binfmt.registrations.x86_64-linux = {
+    interpreter = "${pkgs.box64}/bin/box64";
+    wrapInterpreterInShell = false;
+    magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+    mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+  };
+
   networking.hostName = "thorley";
 
   time.timeZone = "Asia/Seoul";
