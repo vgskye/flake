82817c21e92690065f50f158cf50d84ee42e25df
Skye <me@skye.vg>
2025-05-23 17:46:41 +0900

    fucj

--- thorley/depthcharge/depthchargectl.nix
+++ thorley/depthcharge/depthchargectl.nix
@@ -7,8 +7,8 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "vgskye";
     repo = pname;
-    rev = "94975edf7d451c6506ecc6e4f5516479796b945a";
-    hash = "sha256-SFJWJRtqkh0SCi7KC0olcSVcMfT3dFyWnnLneMrM0bQ=";
+    rev = "501690d1e7adc80b10417d5bf6f1e79d7f9fd0dd";
+    hash = "sha256-oXPX3WfJDzy5LU5GW8Poas3DH+MxMhCsmrgVs5krmj0=";
   };
 
   cargoHash = "sha256-c4T5kL8JEEwG1nOoeQxfAe+KUv5ryRsuu02IjRQJaoE=";
