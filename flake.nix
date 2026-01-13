b92ac65bfd943c5c846eec63e287f024b69e3d95
Skye <me@skye.vg>
2026-01-13 18:48:52 +0900

    dsfaadsfafdsdfas

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
       url = "github:catppuccin/nix";
     };
     quiclime = {
-      url = "git+https://git.skye.vg/me/quiclime.git?ref=glory-to-arstotzka";
+      url = "github:vgskye/e4mc-quiclime/glory-to-arstotzka";
       inputs.nixpkgs.follows = "nixpkgs-unwrapped";
     };
     babysitter = {
