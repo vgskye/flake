fbd1cabeb1deecfb39d21f3e5175a56d407f7c40
Skye <skye@is-quite.gay>
2026-02-01 18:06:59 +0900

    bump iroh relay

--- e4mc/iroh-relay.nix
+++ e4mc/iroh-relay.nix
@@ -2,19 +2,24 @@
   lib,
   fetchFromGitHub,
   rustPlatform,
+  lld,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "iroh-relay";
-  version = "0.95.1";
+  version = "0.96.0";
 
   src = fetchFromGitHub {
     owner = "n0-computer";
     repo = "iroh";
     rev = "v${version}";
-    hash = "sha256-YxifH/mH6x6b8J5xyG+/f18o9ngmiLVKvRaDgIv3ok8=";
+    hash = "sha256-J7FiKIBFUnTUJJwzwzfyk7+CK0UKlAPNFjVDDGlHMqM=";
   };
 
-  cargoHash = "sha256-MdJpGCLf90fTjbJKHCrLLZbLyb4gmQn4SsF5iCqNVVI=";
+  nativeBuildInputs = [
+    lld
+  ];
+
+  cargoHash = "sha256-W8PVysQffGuxBIDpcZ77ujOQ5KBED6svwEXPeZpQmTc=";
 
   buildFeatures = [ "server" ];
   cargoBuildFlags = [
