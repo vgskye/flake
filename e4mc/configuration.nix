f8727199044759e49ba2b47296ade1a19e17f498
Skye <skye@is-quite.gay>
2026-06-09 17:15:02 +0900

    afdsfdas

--- e4mc/configuration.nix
+++ e4mc/configuration.nix
@@ -103,7 +103,7 @@
   services.telegraf.extraConfig = {
     inputs.prometheus = {
       urls = [
-        # "http://127.0.0.1:25585/metrics"
+        "http://127.0.0.1:25585/metrics"
         "http://127.0.0.1:9090/metrics"
       ];
     };
